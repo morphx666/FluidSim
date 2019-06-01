d0164ae492b97f92cd75011184ea46e15d6cf20f
Xavier Flix <xavier@xfx.net>
2019-06-01 18:49:58 -0400

    Add project files.

---
+++ FluidSim/ComplexDouble.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LiquidSim {
+    using System;
+    using System.Numerics;
+
+    public class ComplexDouble {
+        public double R;
+        public double I;
+
+        public static readonly ComplexDouble i = new ComplexDouble(0, 1);
+
+        public ComplexDouble() {
+        }
+
+        public ComplexDouble(double r) {
+            this.R = r;
+        }
+
+        public ComplexDouble(double r, double i) {
+            this.R = r;
+            this.I = i;
+        }
+
+        public double Abs() {
+            return Magnitude();
+        }
+
+        public double Power() {
+            return R * R + I * I;
+        }
+
+        public double Power2() {
+            return Math.Abs(R) + Math.Abs(I);
+        }
+
+        public double Power2Root() {
+            return Math.Sqrt(Power2());
+        }
+
+        public ComplexDouble Conjugate() {
+            return new ComplexDouble(R, -I);
+        }
+
+        public double Magnitude() {
+            return Math.Sqrt(Power());
+        }
+
+        public static ComplexDouble operator +(ComplexDouble n1, ComplexDouble n2) {
+            return new ComplexDouble(n1.R + n2.R, n1.I + n2.I);
+        }
+
+        public static ComplexDouble operator +(ComplexDouble n1, double n2) {
+            return new ComplexDouble(n1.R + n2, n1.I);
+        }
+
+        public static ComplexDouble operator +(double n1, ComplexDouble n2) {
+            return new ComplexDouble(n1 + n2.R, n2.I);
+        }
+
+        public static ComplexDouble operator -(ComplexDouble n1, ComplexDouble n2) {
+            return new ComplexDouble(n1.R - n2.R, n1.I - n2.I);
+        }
+
+        public static ComplexDouble operator -(ComplexDouble n1, double n2) {
+            return new ComplexDouble(n1.R - n2, n1.I);
+        }
+
+        public static ComplexDouble operator -(double n1, ComplexDouble n2) {
+            return new ComplexDouble(n1 - n2.R, n2.I);
+        }
+
+        public static ComplexDouble operator *(ComplexDouble n1, ComplexDouble n2) {
+            return new ComplexDouble(n1.R * n2.R - n1.I * n2.I, n1.I * n2.R + n2.I * n1.R);
+        }
+
+        public static ComplexDouble operator *(ComplexDouble n1, double n2) {
+            return new ComplexDouble(n1.R * n2, n1.I * n2);
+        }
+
+        public static ComplexDouble operator *(double n1, ComplexDouble n2) {
+            return new ComplexDouble(n1 * n2.R, n1 * n2.I);
+        }
+
+        public static ComplexDouble operator /(ComplexDouble n1, ComplexDouble n2) {
+            ComplexDouble c2 = n2.Conjugate();
+            n1 *= c2;
+            n2 *= c2;
+            return n1 / n2.R;
+        }
+
+        public static ComplexDouble operator /(ComplexDouble n1, double n2) {
+            return new ComplexDouble(n1.R / n2, n1.I / n2);
+        }
+
+        public static implicit operator ComplexDouble(double n) {
+            return new ComplexDouble(n);
+        }
+
+        public override string ToString() {
+            if(I >= 0) {
+                return $"{R:N2} + {I:N2}i";
+            } else {
+                return $"{R:N2} - {Math.Abs(I):N2}i";
+            }
+        }
+    }
+}
---
+++ FluidSim/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.IO;
+
+// This is a direct port of a fluid simulation program developed by Yusuke Endoh
+// https://github.com/LeoColomb/FluidASCII/blob/master/endoh1_deobfuscate.c
+
+// http://madebyevan.com/obscure-cpp-features/
+
+namespace LiquidSim {
+    public class Program {
+        private const double G = 1.0; // Gravity factor
+        private const double P = 4.0; // Pressure
+        private const double V = 8.0; // Viscosity
+
+        public static void Main(string[] args) {
+            ComplexDouble[] a = new ComplexDouble[97687];
+            ComplexDouble w = 0, d;
+            int p, q, r = 0;
+            int x, y;
+            char[] b = new char[6856];
+            int t;
+
+            const string prefix = " '`-.|//,\\|\\_\\/#\n";
+
+            Console.CursorVisible = false;
+            int fileIndex = 0;
+            FileInfo[] files = (new DirectoryInfo(@"..\..\FluidASCII")).GetFiles("*.txt");
+            while(true) {
+                Console.Clear();
+                Console.SetCursorPosition(0, 0);
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("Project: ");
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine($"{files[fileIndex].Name.Split('.')[0].ToUpper()}");
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.WriteLine("Press [ESC] to terminate, any other key to run next project");
+                Console.ForegroundColor = ConsoleColor.White;
+                string data = File.ReadAllText(files[fileIndex].FullName).Replace("\r", "");
+                fileIndex = (fileIndex + 1) % files.Length;
+
+                for(int i = 0; i < a.Length; i++) a[i] = new ComplexDouble();
+                r = 0;
+                w = 0;
+
+                for(int i = 0; i < data.Length; i++) {
+                    x = data[i];
+                    // w = x > 10 ? 32 < x ? 4[*r++ = w, r] = w + 1, *r = r[5] = x == 35, r += 9 : 0, w - I : (x = w + 2);
+                    if(x > 10) {
+                        if(32 < x) {
+                            a[r++] = w; // *r++ = w
+                            a[r + 4] = w + 1; // 4[*r++ = w, r] = w + 1
+                            a[r] = a[r + 5] = (x == 35 ? 1 : 0); // *r = r[5] = x == 35
+                            r += 9;
+                        }
+                        w -= ComplexDouble.i;
+                    } else {
+                        w = w.R + 2;
+                    }
+                }
+
+                for(; !Console.KeyAvailable;) {
+                    Console.SetCursorPosition(0, 3);
+                    Console.Write(b);
+
+                    for(p = 0; p < r; p += 5) {
+                        a[p + 2] = a[p + 1] * 9;
+                        for(q = 0; q < r; q += 5) {
+                            w = (a[p] - a[q]).Magnitude() / 2 - 1;
+                            if(0 < (int)(1 - w).R) a[p + 2] += w * w;
+                        }
+                    }
+                    for(p = 0; p < r; p += 5) {
+                        a[p + 3] = G;
+                        for(q = 0; q < r; q += 5) {
+                            w = (d = (a[p] - a[q])).Magnitude() / 2 - 1;
+                            if(0 < (int)(1 - w).R) a[p + 3] += w * (d * (3 - a[p + 2] - a[q + 2]) * P + a[p + 4] * V - a[q + 4] * V) / a[p + 2];
+                        }
+                    }
+                    for(x = 0; 2012 - 1 > x++;) {
+                        b[x] = '\x0';
+                    }
+                    for(p = 0; p < r; p += 5) {
+                        t = 10 + (x = (int)(a[p] * ComplexDouble.i).R) + 80 * (y = (int)(a[p] / 2).R);
+                        a[p] += a[p + 4] += a[p + 3] / 10 * (a[p + 1].Power2() == 0 ? 1 : 0);
+                        // x = 0 <= x && x < 79 && 0 <= y && y < 23 ? 1[1[*t |= 8, t] |= 4, t += 80] = 1, *t |= 2 : 0;
+                        if(0 <= x && x < 79 && 0 <= y && y < 23) {
+                            b[t] = (char)((byte)b[t] | 8); // *t |= 8
+                            b[t + 1] = (char)((byte)b[t + 1] | 4); // 1[*t |= 8, t] |= 4
+                            t += 80;
+                            b[t + 1] = (char)1; // 1[1[*t |= 8, t] |= 4, t += 80] = 1
+                            b[t] = (char)((byte)b[t] | 2); // *t |= 2
+                        }
+                    }
+                    for(x = 0; 2012 - 1 > x++;) {
+                        b[x] = prefix[(x % 80 - 9) != 0 ? b[x] : (prefix.Length - 1)];
+                    }
+                }
+
+                if(Console.ReadKey().Key == ConsoleKey.Escape) return;
+            }
+        }
+    }
+}
